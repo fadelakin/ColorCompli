6173646afeb8e8ba902cddfd14e4d47985a9245e
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-10 20:10:20 -0500

    getting somewhere

--- app/src/main/java/com/fisheradelakin/colorcompli/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/colorcompli/MainActivity.java
@@ -4,18 +4,23 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
     Button mHelpButton;
+    EditText mColorField;
+    Button mGetCompliments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mHelpButton = (Button) findViewById(R.id.getHelpButton);
+        mColorField = (EditText) findViewById(R.id.colorEditText);
+        mGetCompliments = (Button) findViewById(R.id.getComplimentsButton);
+
+        // Help Button opens a dialog of info to the user
         mHelpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +39,25 @@ public class MainActivity extends ActionBarActivity {
                 newFragment.show(getFragmentManager(), "help");
             }
         });
+
+        // Get compliments and change the background color
+        mGetCompliments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mColorField.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Please enter your favorite color", Toast.LENGTH_SHORT).show();
+                } else {
+                    String color = mColorField.getText().toString();
+                    giveCompliments(color);
+                }
+            }
+        });
+    }
+
+    private void giveCompliments(String color) {
+        Intent intent = new Intent(this, ComplimentsActivity.class);
+        intent.putExtra(getString(R.string.key_color), color);
+        startActivity(intent);
     }
 
     public static class HelpDialogFragment extends DialogFragment {
