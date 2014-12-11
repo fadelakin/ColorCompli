893a1bc47adf86920e4ba3462031efd640f72a18
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-10 21:56:52 -0500

    yo

--- app/src/main/java/com/fisheradelakin/colorcompli/ComplimentsActivity.java
+++ app/src/main/java/com/fisheradelakin/colorcompli/ComplimentsActivity.java
@@ -1,39 +1,80 @@
 package com.fisheradelakin.colorcompli;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ComplimentsActivity extends ActionBarActivity {
+
+    Compliments compliment = new Compliments();
+    TextView complimentText;
+    Button getAnotherCompliment;
+    private String mColor;
+
+    RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compliments);
-    }
 
+        Intent intent = getIntent();
+        mColor = intent.getStringExtra(getString(R.string.key_color).toLowerCase());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_compliments, menu);
-        return true;
-    }
+        complimentText = (TextView) findViewById(R.id.complimentsTextView);
+        getAnotherCompliment = (Button) findViewById(R.id.getAnotherCompliment);
+        layout = (RelativeLayout) findViewById(R.id.layout);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(mColor == null) {
+            mColor = "blue";
         }
 
-        return super.onOptionsItemSelected(item);
+        switch (mColor) {
+            case "turquoise":
+                layout.setBackgroundColor(Color.rgb(26, 188, 156));
+                break;
+            case "emerald":
+                layout.setBackgroundColor(Color.rgb(46, 204, 113));
+                break;
+            case "green":
+                layout.setBackgroundColor(Color.GREEN);
+                break;
+            case "blue":
+                layout.setBackgroundColor(Color.rgb(41, 128, 185));
+                break;
+            case "orange":
+                layout.setBackgroundColor(Color.rgb(243, 156, 18));
+                break;
+            case "yellow":
+                layout.setBackgroundColor(Color.YELLOW);
+                break;
+            case "gray":
+                layout.setBackgroundColor(Color.GRAY);
+                break;
+            case "purple":
+                layout.setBackgroundColor(Color.rgb(142, 68, 173));
+                break;
+            case "red":
+                layout.setBackgroundColor(Color.RED);
+                break;
+            default:
+                Toast.makeText(this, "Sorry, but your color isn't available. Reverting to default.", Toast.LENGTH_SHORT);
+                layout.setBackgroundColor(Color.rgb(52, 152, 219));
+                break;
+        }
+        getAnotherCompliment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
