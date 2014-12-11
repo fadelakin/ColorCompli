ec16cc4b367e1d7c5ad7b64e55bb0958b4256710
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-12-10 22:15:16 -0500

    essentially finished

--- app/src/main/java/com/fisheradelakin/colorcompli/ComplimentsActivity.java
+++ app/src/main/java/com/fisheradelakin/colorcompli/ComplimentsActivity.java
@@ -13,10 +13,10 @@ import android.widget.Toast;
 
 public class ComplimentsActivity extends ActionBarActivity {
 
-    Compliments compliment = new Compliments();
+    private Compliments compliment = new Compliments();
     TextView complimentText;
     Button getAnotherCompliment;
-    private String mColor;
+    String mColor;
 
     RelativeLayout layout;
 
@@ -32,47 +32,54 @@ public class ComplimentsActivity extends ActionBarActivity {
         getAnotherCompliment = (Button) findViewById(R.id.getAnotherCompliment);
         layout = (RelativeLayout) findViewById(R.id.layout);
 
-        if(mColor == null) {
-            mColor = "blue";
-        }
-
         switch (mColor) {
             case "turquoise":
                 layout.setBackgroundColor(Color.rgb(26, 188, 156));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(26, 188, 156));
                 break;
             case "emerald":
                 layout.setBackgroundColor(Color.rgb(46, 204, 113));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(46, 204, 113));
                 break;
             case "green":
-                layout.setBackgroundColor(Color.GREEN);
+                layout.setBackgroundColor(Color.rgb(39, 174, 96));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(39, 174, 96));
                 break;
             case "blue":
                 layout.setBackgroundColor(Color.rgb(41, 128, 185));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(41, 128, 185));
                 break;
             case "orange":
-                layout.setBackgroundColor(Color.rgb(243, 156, 18));
+                layout.setBackgroundColor(Color.rgb(211, 84, 0));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(211, 84, 0));
                 break;
             case "yellow":
-                layout.setBackgroundColor(Color.YELLOW);
+                layout.setBackgroundColor(Color.rgb(241, 196, 15));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(241, 196, 15));
                 break;
             case "gray":
                 layout.setBackgroundColor(Color.GRAY);
+                getAnotherCompliment.setBackgroundColor(Color.GRAY);
                 break;
             case "purple":
                 layout.setBackgroundColor(Color.rgb(142, 68, 173));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(142, 68, 173));
                 break;
             case "red":
-                layout.setBackgroundColor(Color.RED);
+                layout.setBackgroundColor(Color.rgb(192, 57, 43));
+                getAnotherCompliment.setBackgroundColor(Color.rgb(192, 57, 43));
                 break;
             default:
-                Toast.makeText(this, "Sorry, but your color isn't available. Reverting to default.", Toast.LENGTH_SHORT);
+                Toast.makeText(this, "Sorry, but your color isn't available. Reverting to default.", Toast.LENGTH_SHORT).show();
                 layout.setBackgroundColor(Color.rgb(52, 152, 219));
                 break;
         }
+
         getAnotherCompliment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String compli = compliment.getCompliment();
+                complimentText.setText(compli);
             }
         });
 
